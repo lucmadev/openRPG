5358023de95e775e158c0976992dbfb7c9b4bc94
Lucma <gianfranco@codigolibre.coop.ar>
2026-07-18 17:06:38 -0300

    [L] Share EXP with party members within 50 blocks and add /rpg alias for /openrpg

--- src/main/kotlin/org/lucma/openRPG/listeners/ExpListener.kt
+++ src/main/kotlin/org/lucma/openRPG/listeners/ExpListener.kt
@@ -8,6 +8,7 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDeathEvent
 import org.lucma.openRPG.core.LanguageManager.msg
+import org.lucma.openRPG.core.PartyManager
 import org.lucma.openRPG.managers.PlayerClassManager
 import org.lucma.openRPG.managers.PlayerDataManager
 
@@ -15,6 +16,9 @@ object ExpListener : Listener {
 
     private val colorExp = TextColor.color(0x55FF55)
     private val colorLevel = TextColor.color(0xFFAA00)
+    private val colorPartyExp = TextColor.color(0x55AAFF)
+
+    private const val PARTY_EXP_RADIUS = 50.0
 
     @EventHandler
     fun onEntityDeath(event: EntityDeathEvent) {
@@ -26,32 +30,57 @@ object ExpListener : Listener {
             val exp = calculateExp(event)
             if (exp <= 0) return
 
-            val leveledUp = PlayerDataManager.addExp(killer, exp)
-            val data = PlayerDataManager.getOrCreate(killer)
+            // Grant EXP to the killer
+            grantExp(killer, exp, false)
 
-            if (leveledUp) {
-                killer.sendMessage(
-                    Component.text(msg("exp.level_up", killer, data.level))
-                        .color(colorLevel)
-                        .append(
-                            Component.text(" " + msg("exp.talent_points_hint", killer, data.talentPoints))
-                                .color(TextColor.color(0xAAAAAA))
-                        )
-                )
-            } else {
-                killer.sendActionBar(
-                    Component.text(msg("exp.gained", killer, exp))
-                        .color(colorExp)
-                        .append(
-                            Component.text(" " + msg("exp.bar_format", killer, data.exp, data.expToNextLevel))
-                                .color(TextColor.color(0x888888))
-                        )
-                )
+            // Grant EXP to all nearby party members
+            val party = PartyManager.getParty(killer)
+            if (party != null) {
+                val killerLoc = killer.location
+                for (member in party.members) {
+                    if (member.uniqueId == killer.uniqueId) continue
+                    if (!member.isOnline) continue
+
+                    val distance = member.location.distance(killerLoc)
+                    if (distance > PARTY_EXP_RADIUS) continue
+
+                    grantExp(member, exp, true)
+                }
             }
 
         } catch (ex: Exception) {
             Bukkit.getLogger().severe("[openRPG] Error in ExpListener: " + ex.message)
             ex.printStackTrace()
+        }
+    }
+
+    private fun grantExp(player: org.bukkit.entity.Player, exp: Int, isPartyShare: Boolean) {
+        val leveledUp = PlayerDataManager.addExp(player, exp)
+        val data = PlayerDataManager.getOrCreate(player)
+
+        if (leveledUp) {
+            player.sendMessage(
+                Component.text(msg("exp.level_up", player, data.level))
+                    .color(colorLevel)
+                    .append(
+                        Component.text(" " + msg("exp.talent_points_hint", player, data.talentPoints))
+                            .color(TextColor.color(0xAAAAAA))
+                    )
+            )
+        } else if (isPartyShare) {
+            player.sendActionBar(
+                Component.text(msg("exp.party_share", player, exp))
+                    .color(colorPartyExp)
+            )
+        } else {
+            player.sendActionBar(
+                Component.text(msg("exp.gained", player, exp))
+                    .color(colorExp)
+                    .append(
+                        Component.text(" " + msg("exp.bar_format", player, data.exp, data.expToNextLevel))
+                            .color(TextColor.color(0x888888))
+                    )
+            )
         }
     }
 
